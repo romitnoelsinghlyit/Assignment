4d2db899d9d8f683a48e20b258fb664a83ebb9b5
romitnoelsinghlyit <l00130450@student.lyit.ie>
2016-11-17 15:07:55 +0000

    dasboard

--- Login/Login/Dashboard.xaml.cs
+++ Login/Login/Dashboard.xaml.cs
@@ -19,9 +19,92 @@ namespace Login
     /// </summary>
     public partial class Dashboard : Window
     {
+        public User user = new User();
         public Dashboard()
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void frmeMainFrame_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+
+        }
+
+        private void btnPatients_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSearchText_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnResetSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEmergency_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnElective_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnGeneralWard_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSurgicalWard_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnICU_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOrthopaedicWard_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPaediatricWard_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnTestsScans_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBilling_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdministration_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            Environment.Exit(0);
+        }
+
     }
 }
--- Login/Login/MainWindow.xaml.cs
+++ Login/Login/MainWindow.xaml.cs
@@ -20,9 +20,70 @@ namespace Login
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<User> userList = new List<User>();
+
+        HMDatabaseEntities dbEntities = new HMDatabaseEntities();
+
         public MainWindow()
         {
             InitializeComponent();
+            tbxUserID.Focus();
+        }
+
+        private void mtdLoadUsers()
+        {
+            userList.Clear();
+            foreach (var user in dbEntities.Users)
+            {
+                userList.Add(user);
+
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            Environment.Exit(0);
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            User userDetails = new User();
+            string currentUser = tbxUserID.Text.Trim();
+            string currentPassword = tbxPassword.Password;
+            userDetails = mtdGetUserDetails(currentUser, currentPassword);
+            if (userDetails.AccessLevel > 0)
+            {
+                this.Hide();
+                Dashboard dashBoard = new Dashboard();
+                dashBoard.user = userDetails;
+                dashBoard.ShowDialog();
+            }
+            else
+            {
+                lblLoginAdvice.Content = "Invalid details!";
+                tbxUserID.Text = "";
+                tbxPassword.Password = "";
+            }
+        }
+
+        private User mtdGetUserDetails(string userID, string password)
+        {
+            User userDetails = new User();
+            foreach (var user in userList)
+            {
+                if (userID == user.UserID && password == user.Password)
+                {
+                    userDetails = user;
+                }
+            }
+            return userDetails;
         }
     }
 }
+
